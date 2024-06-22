fa13a88675578065a9f7271a5e0ff00fbc531909
aruna-o <aruna-me@outlook.com>
2024-06-22 19:03:44 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+/*
+    Initialize the Database connection
+*/
+
+async function connect() {
+    try {
+        await mongoose.connect(process.env.URI);
+        console.log('connected to mongodb');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+connect();
+
+/* 
+    APIs
+*/
+
+app.get('/', (req, res) => {
+    res.send({message: 'Hello Word'})
+})
+
+/*
+    Initialize the Node.js server
+*/
+
+app.listen(process.env.PORT, () => {
+    console.log('Node server is started');
+})
